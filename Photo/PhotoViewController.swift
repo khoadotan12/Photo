aa3df099f71ac5f39911c087ee96dca23fade087
Đỗ Tấn Khoa <khoadt2@vng.com.vn>
2019-04-08 21:30:22 +0700

    add load image from url

--- Photo/PhotoViewController.swift
+++ Photo/PhotoViewController.swift
@@ -14,25 +14,25 @@ class PhotoViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         let width = (view.frame.size.width - 20) / 3
         let layout = collectionView.collectionViewLayout as!
-            UICollectionViewFlowLayout
+        UICollectionViewFlowLayout
         layout.itemSize = CGSize(width: width, height: width)
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
 
 
@@ -43,6 +43,10 @@ extension PhotoViewController {
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath)
+        if let imageView = cell.viewWithTag(10) as? UIImageView {
+            let url = URL(string: "https://salt.tikicdn.com/cache/w1200/ts/product/ad/6c/a9/fd6450656e968244cf43cabf55a3cbb0.jpg")
+            imageView.load(url: url!)
+        }
         return cell
     }
     
@@ -51,6 +55,20 @@ extension PhotoViewController {
             print(scrollView.contentOffset.y)
             print(scrollView.contentSize.height)
             print(view.frame.size.height)
+        }
+    }
+}
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
         }
     }
 }
